3413447bf725bae5aadf43300282d89e79bb289f
Den <shapkinden@gmail.com>
2017-07-07 18:42:39 +0300

    Fix build via https://github.com/adazzle/react-data-grid/issues/675#issuecomment-308442896

--- config/webpack.js
+++ config/webpack.js
@@ -20,7 +20,8 @@ module.exports = function (release) {
     debug: !release,
     devtool: false,
     entry: {
-      reactAutoComplete : './src/index.js',
+      'reactAutoComplete': './src/index.js',
+      'reactAutoComplete.min': './src/index.js',
     },
     output: {
       path: './lib/',
@@ -51,10 +52,12 @@ module.exports = function (release) {
       reasons: !release
     },
 
-    plugins: release ? [,
+    plugins: release ? [
       new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
       new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        include: /\.min\.js$/,
+      }),
       new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.optimize.AggressiveMergingPlugin()
     ] : [],
